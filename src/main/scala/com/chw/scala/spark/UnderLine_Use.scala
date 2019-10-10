48861d60c7e5c9181fa395bf11db321ce7f83d8d
chaihongwei <chaihongwei@hxkid.com>
2019-10-10 18:08:19 +0800

    测试下划线11种用法

---
+++ src/main/scala/com/chw/scala/spark/UnderLine_Use.scala
@@ -0,0 +1,37 @@
+package com.chw.scala.spark
+
+import com.chw.scala.spark.MapWithState_UpdateStateByKey.Foo
+
+object UnderLine_Use extends App {
+
+  def printArgs(ages: Int*) {
+  }
+
+  printArgs(Array(1, 2, 3): _*)
+
+  def printList(list: List[_]): Unit = {
+    list.foreach(println)
+  }
+
+  class Foo {
+    def name = {
+      "foo"
+    }
+
+    //name_=相当于方法名称
+    def name_=(str: String) {
+      println("set name " + str)
+    }
+
+  }
+
+  val m = new Foo()
+  m.name = "Foo"
+  m.name_=("Foo")
+
+  def sum(a: Int, b: Int, c: Int) = a + b + c
+  val b = sum(1, _:Int, 3)
+  println(b(2))
+
+}
+
